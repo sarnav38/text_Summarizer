3ba314eafdd5f3be22de7f3628215fed79261a53
Arnav Sharma <66561012+sarnav38@users.noreply.github.com>
2023-06-21 19:34:28 +0530

    Update popup.js

--- client/popup/popup.js
+++ client/popup/popup.js
@@ -15,11 +15,11 @@ btn.onclick = async (e)=>{
     let youtube_url = `https://www.youtube.com/watch?v=${videoId}`
 
     // for NGROK Run paste ngrok create link below at every run ngrok chg url
-    // let ngrok = 'https://2e85-103-95-83-57.ngrok-free.app'
-    // let api_url = `${ngrok}/textSum?youtube_url='${youtube_url}'`
+    let ngrok = 'ngrok url'
+    let api_url = `${ngrok}/textSum?youtube_url='${youtube_url}'`
 
     // paste server url below...below used is deploy with aws lambda
-    let api_url = `https://tguch7y3xu72nbbxcv4hhqaquu0pfdqv.lambda-url.us-east-1.on.aws/?name=${videoId}`
+    // let api_url = `server_url/?youtube_url='${videoId}'`
     await chrome.tabs.sendMessage(tabs[0].id, {action:"loading",youtube_url:youtube_url ,data:"fetching from server"})
     fetchData(api_url).then((result)=>{
         // console.log('result')
